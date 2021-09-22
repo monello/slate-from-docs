043c3df245be6f133ffcff2a5fb64f3f3b4747b7
Morne Louw <morne.louw@saongroup.com>
2021-09-22 06:20:45 +0200

    Add the required imports to get a basic Slate instance running inside the Editor component

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,3 +1,11 @@
+// Import React dependencies.
+import React, { useMemo, useState } from 'react'
+// Import the Slate editor factory.
+import { createEditor } from 'slate'
+
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
+
 const Editor = () => <h1>Hello World</h1>
 
 export default Editor;
