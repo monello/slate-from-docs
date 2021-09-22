c8a1561116f3dc77965a951a51c9883af43a148f
Morne Louw <morne.louw@saongroup.com>
2021-09-22 06:33:09 +0200

    Tidied up the code. Comments cleanup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// Components
 import Editor from "./components/Editor/Editor";
 
 function App() {
--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,16 +1,12 @@
-// Import React dependencies.
+// React
 import React, { useMemo, useState } from 'react'
-// Import the Slate editor factory.
-import { createEditor } from 'slate'
 
-// Import the Slate components and React plugin.
+// SlateJS
+import { createEditor } from 'slate'
 import { Slate, Editable, withReact } from 'slate-react'
 
 const Editor = () => {
-    // Create a Slate editor object that won't change across renders.
     const editor = useMemo(() => withReact(createEditor()), [])
-
-    // Keep track of state for the value of the editor.
     const [value, setValue] = useState([
         {
             type: 'paragraph',
@@ -18,8 +14,6 @@ const Editor = () => {
         },
     ])
 
-    // Render the Slate context.
-    // Add the <Editable /> component inside the context.
     return (
         <Slate
             editor={editor}
