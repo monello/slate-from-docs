f2157b2f9b244916b979f96a8c312c085e168423
Morne Louw <morne.louw@saongroup.com>
2021-09-22 06:23:44 +0200

    Add state manegement to the component

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -10,6 +10,9 @@ const Editor = () => {
     // Create a Slate editor object that won't change across renders.
     const editor = useMemo(() => withReact(createEditor()), [])
 
+    // Keep track of state for the value of the editor.
+    const [value, setValue] = useState([])
+
     return <h1>Hello World</h1>
 }
 
